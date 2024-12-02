c03223eb51a59c0676ce0bd5f965a3fa6604d102
lizob23 <lizobly@protonmail.com>
2024-12-02 17:11:50 +0700

    init echo hello world

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load("config.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
