fb0e438881c045524f53139b2060dd7104fc5d6c
lizob23 <lizobly@protonmail.com>
2024-12-07 18:08:37 +0700

    init api response

---
+++ internal/rest/controller.go
@@ -0,0 +1,34 @@
+package rest
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Controller struct {
+}
+
+type StandardAPIResponse struct {
+	Message  string      `json:"message"`
+	Data     interface{} `json:"data"`
+	Errors   interface{} `json:"errors"`
+	Metadata interface{} `json:"metadata"`
+}
+
+func (c Controller) Ok(ctx echo.Context, message string, data, metadata interface{}) error {
+
+	return ctx.JSON(http.StatusOK, StandardAPIResponse{
+		Message:  message,
+		Data:     data,
+		Metadata: metadata,
+	})
+}
+
+func (c Controller) ResponseError(ctx echo.Context, httpStatus int, message string, errorData interface{}) error {
+
+	return ctx.JSON(httpStatus, StandardAPIResponse{
+		Message: message,
+		Errors:  errorData,
+	})
+}
---
+++ internal/rest/traveller_handler.go
@@ -0,0 +1,96 @@
+package rest
+
+import (
+	"lizobly/cotc-db/domain"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+type TravellerService interface {
+	GetByID(ctx echo.Context, id int) (res domain.Traveller, err error)
+	Create(ctx echo.Context, input *domain.Traveller) (err error)
+	Update(ctx echo.Context, input *domain.Traveller) (err error)
+	Delete(ctx echo.Context, id int) (err error)
+}
+
+type TravellerHandler struct {
+	Controller
+	Service TravellerService
+}
+
+func NewTravellerHandler(e *echo.Echo, svc TravellerService) {
+	handler := &TravellerHandler{
+		Service: svc,
+	}
+	e.GET("/travellers/:id", handler.GetByID)
+	e.POST("/travellers", handler.Create)
+	e.PUT("/travellers/:id", handler.Update)
+	e.DELETE("/travellers/:id", handler.Delete)
+}
+
+func (a *TravellerHandler) GetByID(ctx echo.Context) error {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		return a.ResponseError(ctx, http.StatusBadRequest, "error validation", "id not found")
+	}
+
+	traveller, err := a.Service.GetByID(ctx, id)
+	if err != nil {
+		return a.ResponseError(ctx, http.StatusBadRequest, "error get data", err.Error())
+	}
+
+	return a.Ok(ctx, "success", traveller, nil)
+}
+
+func (a *TravellerHandler) Create(ctx echo.Context) error {
+
+	var newTraveller domain.Traveller
+	err := ctx.Bind(&newTraveller)
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	err = a.Service.Create(ctx, &newTraveller)
+	if err != nil {
+		return a.ResponseError(ctx, http.StatusBadRequest, "error get data", err.Error())
+	}
+
+	return a.Ok(ctx, "success", newTraveller, nil)
+}
+
+func (a *TravellerHandler) Update(ctx echo.Context) error {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		return a.ResponseError(ctx, http.StatusBadRequest, "error validation", "id not found")
+	}
+
+	var traveller domain.Traveller
+	err = ctx.Bind(&traveller)
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, err.Error())
+	}
+	traveller.ID = int64(id)
+
+	err = a.Service.Update(ctx, &traveller)
+	if err != nil {
+		return a.ResponseError(ctx, http.StatusBadRequest, "error get data", err.Error())
+	}
+
+	return a.Ok(ctx, "success", traveller, nil)
+}
+
+func (a *TravellerHandler) Delete(ctx echo.Context) error {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		return a.ResponseError(ctx, http.StatusBadRequest, "error validation", "id not found")
+	}
+
+	err = a.Service.Delete(ctx, id)
+	if err != nil {
+		return a.ResponseError(ctx, http.StatusBadRequest, "error get data", err.Error())
+	}
+
+	return a.Ok(ctx, "success", nil, nil)
+}
